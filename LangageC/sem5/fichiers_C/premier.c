ba4f3674f686700739b896796a76d5aaa5eaa650
Héloïse Lafargue <102029307+HeloiseLafargue@users.noreply.github.com>
2022-11-17 14:05:41 +0100

    Delete LangageC directory

--- LangageC/sem5/fichiers_C/premier.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h> 
-#include <stdio.h>
-
-int main(){
-    printf("******************************\n");
-    printf("******** Langage C ***********\n");
-    printf("******************************\n");
-    return EXIT_SUCCESS;
-}
