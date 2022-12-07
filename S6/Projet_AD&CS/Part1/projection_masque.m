d4781be57f784f280b243e9757a6e52f7ba75c16
Héloïse Lafargue <102029307+HeloiseLafargue@users.noreply.github.com>
2022-12-07 16:25:06 +0100

    Add files via upload

---
+++ S6/Projet_AD&CS/Part1/projection_masque.m
@@ -0,0 +1,49 @@
+clear all;
+close all;
+
+load eigenfaces_masque;
+
+h = figure('Position',[0,0,0.67*L,0.67*H]);
+figure('Name','RMSE en fonction du nombre de composantes principales','Position',[0.67*L,0,0.33*L,0.3*L]);
+
+% Calcul de la RMSE entre images originales et images reconstruites :
+RMSE_max = 0;
+
+% Composantes principales des données d'apprentissage
+C = X_centre*vp;
+
+ for q = 0:n-1
+     C_q = C(:, 1:q);		% q premières composantes principales
+     vp_q = vp(:, 1:q);		% q premières eigenfaces
+     X_reconstruit = C_q*transpose(vp_q) + individu_moyen;
+     figure(1);
+     set(h,'Name',['Utilisation des ' num2str(q) ' premieres composantes principales']);
+     colormap gray;
+     hold off;
+     for k = 1:n
+         subplot(nb_personnes_base, nb_postures_base,k);
+         img = reshape(X_reconstruit(k,:), size(img));
+         imagesc(img);
+         hold on;
+         axis image;
+         axis off;
+     end
+     
+     figure(2);
+     hold on;
+     
+     RMSE = sqrt(mean((X_masque(:) - X_reconstruit(:)).^2));
+     %RMSE = sqrt((1/numel(X))*sum(( X - X_reconstruit).^2, 'all'));
+     RMSE_max = max(RMSE,RMSE_max);
+
+     plot(q,RMSE,'r+','MarkerSize',8,'LineWidth',2);
+     axis([0 n-1 0 1.1*RMSE_max]);
+     set(gca,'FontSize',20);
+     hx = xlabel('$q$','FontSize',30);
+     set(hx,'Interpreter','Latex');
+     ylabel('RMSE','FontSize',30);
+     
+     pause(0.01);
+ end
+
+% save projection;
