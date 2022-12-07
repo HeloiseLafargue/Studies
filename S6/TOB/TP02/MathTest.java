e6367b386dd5bcabe2280ec05a92c923f2cfb32b
Héloïse Lafargue <102029307+HeloiseLafargue@users.noreply.github.com>
2022-12-07 16:35:35 +0100

    Add files via upload

---
+++ S6/TOB/TP02/MathTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import java.lang.AssertionError;
+
+public class MathTest {
+
+	@Test
+	public void testSqrt() {
+		assertTrue(Math.sqrt(4) == 2);
+	}
+
+}
